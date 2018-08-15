610171ba0700b7bb992eb593f059ef07ec42b240
Felix Holz <felix.holz@me.com>
2018-08-15 18:06:35 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/felix/Desktop/ORB_SLAM2/src/Converter.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Converter.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/Frame.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Frame.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/FrameDrawer.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/FrameDrawer.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/Initializer.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Initializer.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/KeyFrame.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/KeyFrame.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/KeyFrameDatabase.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/KeyFrameDatabase.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/LocalMapping.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/LocalMapping.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/LoopClosing.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/LoopClosing.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/Map.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Map.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/MapDrawer.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/MapDrawer.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/MapPoint.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/MapPoint.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/ORBextractor.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/ORBextractor.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/ORBmatcher.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/ORBmatcher.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/Optimizer.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Optimizer.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/PnPsolver.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/PnPsolver.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/Sim3Solver.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Sim3Solver.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/System.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/System.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/Tracking.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Tracking.cc.o"
+  "/home/felix/Desktop/ORB_SLAM2/src/Viewer.cc" "/home/felix/Desktop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "../"
+  "../include"
+  "/usr/local/include/eigen3"
+  "/home/felix/Desktop/Pangolin/include"
+  "/home/felix/Desktop/Pangolin/build/src/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
