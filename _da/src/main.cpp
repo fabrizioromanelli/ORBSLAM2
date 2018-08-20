2c4e262667033eae503c6cda20270d0953d06d46
Felix Holz <felix.holz@me.com>
2018-08-20 11:21:04 +0200

    release configuration

--- _da/src/main.cpp
+++ _da/src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char ** argv)
 {
 
     std::cout << "werner" << std::endl;
-    
+
 
 }
 
---
+++ cmake-build-release/CMakeFiles/DA.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DA.
+# This may be replaced when dependencies are built.
