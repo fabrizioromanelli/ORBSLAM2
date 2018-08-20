f3fc92e7519b18bec2859abb560bd9b86b1ce822
Felix Holz <felix.holz@me.com>
2018-08-20 21:37:11 +0200

    tracking with smartphone camera feed somewhat working

--- _da/src/main.cpp
+++ _da/src/main.cpp
@@ -2,16 +2,87 @@
 // Created by felix on 20.08.18.
 //
 
+
 #include <iostream>
+#include <chrono>
+#include <unistd.h>
+
 #include <ORB_SLAM2/System.h>
 
+#include "opencv2/opencv.hpp"
 
-int main(int argc, char ** argv)
+
+using namespace cv;
+
+const std::string path_vocab = "/home/felix/Desktop/ORB_SLAM2/Vocabulary/ORBvoc.txt";
+const std::string path_yaml = "/home/felix/Desktop/ORB_SLAM2/Examples/Monocular/KITTI00-02.yaml";
+const std::string path_dataset = "/home/felix/Desktop/kitti/dataset/sequences/00";
+
+
+int main(int, char**)
 {
+    VideoCapture cap("http://10.0.0.8:8080/video");
 
-    std::cout << "werner" << std::endl;
+    if(!cap.isOpened())
+        return -1;
 
 
+    ORB_SLAM2::System slam(path_vocab, path_yaml, ORB_SLAM2::System::MONOCULAR,true);
+
+    Mat curr_frame;
+    auto start_time = std::chrono::steady_clock::now();
+    while(true)
+    {
+        cap >> curr_frame;
+        cvtColor(curr_frame, curr_frame, COLOR_BGR2GRAY);
+
+
+        auto now = std::chrono::steady_clock::now();
+        auto curr_time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(now-start_time).count();
+        double curr_time = curr_time_ms / 1000.0f;
+
+        std::cout << curr_time << std::endl;
+
+        imshow("werner", curr_frame);
+        slam.TrackMonocular(curr_frame, curr_time);
+
+
+        if(waitKey(1) >= 0) break;
+    }
+
+    slam.Shutdown();
+
+    cap.release();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    Mat edges;
+    namedWindow("edges",1);
+    for(;;)
+    {
+        Mat frame;
+        cap >> frame;
+        cvtColor(frame, edges, COLOR_BGR2GRAY);
+        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+        Canny(edges, edges, 0, 30, 3);
+        imshow("edges", edges);
+        if(waitKey(30) >= 0) break;
+    }
+*/
+
+    destroyAllWindows();
+    return 0;
 }
-
-
