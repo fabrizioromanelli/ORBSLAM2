21d8d0e6a482a36f30b7867499897ab14263a737
Fabrizio Romanelli <fabrizio.romanelli@applaudart.com>
2020-01-22 16:15:09 +0100

    Migration to OpenCV 4.2.0 completed

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j8
+make $1
 
 cd ../../g2o
 
@@ -13,7 +13,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j8
+make $1
 
 cd ../../../
 
@@ -28,7 +28,7 @@ echo "Configuring and building ORB_SLAM2 ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j8
+make $1
 
 cd ..
 
