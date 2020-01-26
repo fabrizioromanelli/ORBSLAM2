3a84fcbf52e5ee3f4c6ae68db602818a0885d8ac
Fabrizio Romanelli <fabrizio.romanelli@gmail.com>
2020-01-26 12:41:47 +0100

    Adding calibration for SonyVaio integrated webcam

--- Test/Test.cc
+++ Test/Test.cc
@@ -52,47 +52,12 @@ int main(int argc, char **argv)
             return 1;
         }
 
-// #ifdef COMPILEDWITHC11
-//         std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
-// #else
-//         std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
-// #endif
-
         // Pass the image to the SLAM system
         SLAM.TrackMonocular(resized, currentTs);
-
-// #ifdef COMPILEDWITHC11
-//         std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-// #else
-//         std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
-// #endif
-
-//         double ttrack = std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
-
-//         // Wait to load the next frame
-//         double T = 0;
-//         if(ni  <nImages-1)
-//             T = vTimestamps[ni+1] - tframe;
-//         else if(ni>0)
-//             T = tframe - vTimestamps[ni-1];
-
-//         if(ttrack < T)
-//             usleep((T - ttrack)*1e6);
     }
 
     // Stop all threads
     SLAM.Shutdown();
-
-    // // Tracking time statistics
-    // sort(vTimesTrack.begin(), vTimesTrack.end());
-    // float totaltime = 0;
-    // for(int ni=0; ni<nImages; ni++)
-    // {
-    //     totaltime += vTimesTrack[ni];
-    // }
-    // cout << "-------" << endl << endl;
-    // cout << "median tracking time: " << vTimesTrack[nImages/2] << endl;
-    // cout << "mean tracking time: " << totaltime/nImages << endl;
 
     // Save camera trajectory
     // SLAM.SaveTrajectory("CameraTrajectory.dat");
