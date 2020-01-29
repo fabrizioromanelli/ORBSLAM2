f0a6c84e6ba80833190dfcac785fca70f3de8ac3
Fabrizio Romanelli <fabrizio.romanelli@gmail.com>
2020-01-29 21:53:52 +0100

    Fixing paths for benchmark script

--- Benchmarks/executeBenchmarks.sh
+++ Benchmarks/executeBenchmarks.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 mkdir Results
-./Benchmarks/fromMonocularFile/benchmark_mono_file Vocabulary/ORBvoc.bin Config/Mono-EuRoC.yaml ../Datasets/EuRoC/MH_05/mav0/cam0/data Config/Mono-EuRoC_TimeStamps/MH05.txt > Results/SLAM_monocular_euroc_MH05_file.dat
-./Benchmarks/fromMonocularFile/benchmark_mono_file Vocabulary/ORBvoc.bin Config/Mono-EuRoC.yaml ../Datasets/EuRoC/MH_05/mav0/cam0/data Config/Mono-EuRoC_TimeStamps/MH05.txt > Results/LOC_monocular_euroc_MH05_file.dat
-./Benchmarks/fromRGBDFile/benchmark_rgbd_file Vocabulary/ORBvoc.bin Config/RGB-D-TUM2.yaml ../Datasets/TUM/rgbd_dataset_freiburg2_pioneer_slam Config/RGB-D-associations/fr2_pioneer_slam.txt > Results/SLAM_rgbd_tum_rgbd_dataset_freiburg2_pioneer_slam_file.dat
-./Benchmarks/fromRGBDFile/benchmark_rgbd_file Vocabulary/ORBvoc.bin Config/RGB-D-TUM2.yaml ../Datasets/TUM/rgbd_dataset_freiburg2_pioneer_slam Config/RGB-D-associations/fr2_pioneer_slam.txt > Results/LOC_rgbd_tum_rgbd_dataset_freiburg2_pioneer_slam_file.dat
-./Benchmarks/fromStereoFile/benchmark_stereo_file Vocabulary/ORBvoc.bin Config/Stereo-KITTI04-12.yaml ../Datasets/KITTI/data_odometry_gray/dataset/sequences/15 > Results/SLAM_stereo_kitti_15_file.dat
-./Benchmarks/fromStereoFile/benchmark_stereo_file Vocabulary/ORBvoc.bin Config/Stereo-KITTI04-12.yaml ../Datasets/KITTI/data_odometry_gray/dataset/sequences/15 > Results/LOC_stereo_kitti_15_file.dat
+./fromMonocularFile/benchmark_mono_file ../Vocabulary/ORBvoc.bin ../Config/Mono-EuRoC.yaml ../../Datasets/EuRoC/MH_05/mav0/cam0/data ../Config/Mono-EuRoC_TimeStamps/MH05.txt > Results/SLAM_monocular_euroc_MH05_file.dat
+cp *.map Results/
+./fromMonocularFile/benchmark_mono_file ../Vocabulary/ORBvoc.bin ../Config/Mono-EuRoC.yaml ../../Datasets/EuRoC/MH_05/mav0/cam0/data ../Config/Mono-EuRoC_TimeStamps/MH05.txt > Results/LOC_monocular_euroc_MH05_file.dat
+rm -rf *.map
+./fromRGBDFile/benchmark_rgbd_file ../Vocabulary/ORBvoc.bin ../Config/RGB-D-TUM3.yaml ../../Datasets/TUM/rgbd_dataset_freiburg3_long_office_household ../Config/RGB-D-associations/fr3_long_office_household.txt > Results/SLAM_rgbd_tum_rgbd_dataset_freiburg3_long_office_household_file.dat
+cp *.map Results/
+./fromRGBDFile/benchmark_rgbd_file ../Vocabulary/ORBvoc.bin ../Config/RGB-D-TUM3.yaml ../../Datasets/TUM/rgbd_dataset_freiburg3_long_office_household ../Config/RGB-D-associations/fr3_long_office_household.txt > Results/LOC_rgbd_tum_rgbd_dataset_freiburg3_long_office_household_file.dat
+rm -rf *.map
+./fromStereoFile/benchmark_stereo_file ../Vocabulary/ORBvoc.bin ../Config/Stereo-KITTI04-12.yaml ../../Datasets/KITTI/data_odometry_gray/dataset/sequences/15 > Results/SLAM_stereo_kitti_15_file.dat
+cp *.map Results/
+./fromStereoFile/benchmark_stereo_file ../Vocabulary/ORBvoc.bin ../Config/Stereo-KITTI04-12.yaml ../../Datasets/KITTI/data_odometry_gray/dataset/sequences/15 > Results/LOC_stereo_kitti_15_file.dat
+rm -rf *.map
 cd ..
