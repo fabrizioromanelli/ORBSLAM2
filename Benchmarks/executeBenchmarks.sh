6b7c5d8b98d1d8ec09d04d628da73516dc7de8ba
Fabrizio Romanelli <fabrizio.romanelli@gmail.com>
2020-02-04 23:44:24 +0100

    Finalizing tests on video stream

--- Benchmarks/executeBenchmarks.sh
+++ Benchmarks/executeBenchmarks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir Results
+mkdir -p Results
 ./fromMonocularFile/benchmark_mono_file ../Vocabulary/orb_mur.fbow ../Config/Mono-EuRoC.yaml ../../Datasets/EuRoC/MH_05/mav0/cam0/data ../Config/Mono-EuRoC_TimeStamps/MH05.txt > Results/SLAM_monocular_euroc_MH05_file.dat
 ./fromMonocularFile/benchmark_mono_file ../Vocabulary/orb_mur.fbow ../Config/Mono-EuRoC.yaml ../../Datasets/EuRoC/MH_05/mav0/cam0/data ../Config/Mono-EuRoC_TimeStamps/MH05.txt > Results/LOC_monocular_euroc_MH05_file.dat
 ./fromRGBDFile/benchmark_rgbd_file ../Vocabulary/orb_mur.fbow ../Config/RGB-D-TUM3.yaml ../../Datasets/TUM/rgbd_dataset_freiburg3_long_office_household ../Config/RGB-D-associations/fr3_long_office_household.txt > Results/SLAM_rgbd_tum_rgbd_dataset_freiburg3_long_office_household_file.dat
