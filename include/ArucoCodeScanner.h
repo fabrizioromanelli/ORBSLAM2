ad0a46ea21a0b6635cb2192cf26923dd176c7f45
Fabrizio Romanelli <fabrizio.romanelli@gmail.com>
2021-04-07 15:47:43 +0200

    WIP

--- include/ArucoCodeScanner.h
+++ include/ArucoCodeScanner.h
@@ -8,15 +8,11 @@
 #ifndef ARUCO_CODE_SCANNER_H
 #define ARUCO_CODE_SCANNER_H
 
-#include <opencv2/objdetect.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
-#include "QrCode.h"
+#include "ArucoCode.h"
 
 namespace ORB_SLAM2
 {
@@ -27,33 +23,22 @@ public:
   ArucoCodeScanner();
   // Main scanner function. It is independent of the input sensor.
   void Scan(cv::Mat);
-  void Track(cv::Mat, cv::Point2d);
   cv::Point Detect(cv::Mat);
-  bool getDecodedData(std::string &);
-  std::vector<QrCode> * getQrCodeList();
   cv::Mat getBoundingBox();
-  cv::Mat getRectifiedImage();
   cv::Point getBoundingBoxCenter();
-  void setThresholds(unsigned int, unsigned int);
-  bool isInsideBbox();
   void display();
-  void addQrCodeToMap(QrCode);
-  void loadQrCodeList();
-  void saveQrCodeList();
-  cv::Point getPositionFromCode(std::string);
+  void loadArucoCodeList();
 
 protected:
   cv::Mat inputImage;
-  unsigned int imgWidth;
-  unsigned int imgHeight;
 
 private:
-  cv::QRCodeDetector *qrDecoder;
-  cv::Mat bbox, rectifiedImage;
-  std::string decodedData;
-  cv::Point qrCenter;
-  cv::Rect thRect;
-  std::vector<QrCode> qrCodes;
+  std::vector<int> markerIds;
+  std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
+  cv::Ptr<cv::aruco::DetectorParameters> parameters;
+  cv::Ptr<cv::aruco::Dictionary> dictionary;
+  cv::Point arucoCenter;
+  std::vector<ArucoCode> arucoCodes;
 };
 
 } //namespace ORB_SLAM
