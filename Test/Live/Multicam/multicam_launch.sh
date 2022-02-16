d345bd08869d26e83c8f66fe82782a5b54e87f86
Fabrizio Romanelli <fabrizio.romanelli@gmail.com>
2022-02-16 18:56:01 +0100

    WIP

--- Test/Live/Multicam/multicam_launch.sh
+++ Test/Live/Multicam/multicam_launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 thisDir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-$thisDir/multicam_live ~/workspace/ORBSLAM2/Vocabulary/orb_mur.fbow ~/workspace/ORBSLAM2/Config/RealSense-D435i-MULTI.yaml OFF OFF OFF
+$thisDir/multicam_live ~/workspace/ORBSLAM2/Vocabulary/orb_mur.fbow ~/workspace/ORBSLAM2/Config/RealSense-D435i-MULTI.yaml OFF ON OFF
