dfed74c41ebdb3cb35de6ef4cfd38e869af876f6
Roberto Masocco <robmasocco@gmail.com>
2022-03-16 16:47:56 +0000

    fix: set branches and tags, don't install recommended pkgs
    @fabrizioromanelli double-check.

--- installDependencies.sh
+++ installDependencies.sh
@@ -1,51 +1,79 @@
 #!/bin/bash
 
 # Packages needed by Pangolin
-sudo apt -y install libgl1-mesa
-sudo apt -y install libglew-dev
-sudo apt -y install cmake
-sudo apt -y install libpython2.7-dev
-sudo apt -y install pkg-config
-sudo apt -y install libegl1-mesa-dev libwayland-dev libxkbcommon-dev wayland-protocols
-sudo apt -y install ffmpeg libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libavdevice-dev
+sudo apt-get install -y --no-install-recommends \
+  cmake \
+  ffmpeg \
+  libavcodec-dev \
+  libavdevice-dev \
+  libavformat-dev \
+  libavutil-dev \
+  libegl1-mesa-dev \
+  libgl1-mesa \
+  libglew-dev \
+  libpython2.7-dev \
+  libswscale-dev \
+  libwayland-dev \
+  libxkbcommon-dev \
+  pkg-config \
+  wayland-protocols
 
-cd ~/workspace
+cd ~/workspace || return 1
+
 # Pangolin installation
-git clone https://github.com/stevenlovegrove/Pangolin.git
-cd Pangolin
+git clone --single-branch --branch 'v0.6' --depth 1 https://github.com/stevenlovegrove/Pangolin.git
+cd Pangolin || return 1
 mkdir build
-cd build
+cd build || return 1
 cmake ..
 cmake --build .
-sudo make install -j8
+sudo make install "-j$(nproc --all)"
 
-cd ~/workspace
+cd ~/workspace || return 1
+
 # Eigen installation
-git clone https://gitlab.com/libeigen/eigen.git
-cd eigen
+git clone --single-branch --branch '3.4.0' --depth 1 https://gitlab.com/libeigen/eigen.git
+cd eigen || return 1
 mkdir build
-cd build
+cd build || return 1
 cmake ..
-sudo make install -j8
+sudo make install "-j$(nproc --all)"
 
-# This add libjasper-dev old repo
+cd ~/workspace || return 1
+
+# This adds libjasper-dev old repo
 sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
-sudo apt update
-sudo apt install libjasper1 libjasper-dev
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends \
+  libjasper1 \
+  libjasper-dev
 
 # Packages needed by OpenCV
-sudo apt-get -y install build-essential
-sudo apt-get -y install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get -y install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
+sudo apt-get install -y --no-install-recommends \
+  build-essential \
+  cmake \
+  git \
+  libavcodec-dev \
+  libavformat-dev \
+  libdc1394-22-dev
+  libgtk2.0-dev \
+  libjpeg-dev \
+  libpng-dev \
+  libswscale-dev \
+  libtbb2 \
+  libtbb-dev \
+  libtiff-dev \
+  pkg-config \
+  python-dev \
+  python-numpy
 
-cd ~/workspace
 # OpenCV installation
-git clone git@github.com:opencv/opencv.git
-cd opencv
+git clone --single-branch --branch '4.5.5' --depth 1 https://github.com/opencv/opencv.git
+cd opencv || return 1
 mkdir build
-cd build
+cd build || return 1
 cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
-sudo make install -j8
+sudo make install "-j$(nproc --all)"
 
 # Boost installation
-sudo apt-get install libboost-all-dev
+sudo apt-get install -y --no-install-recommends libboost-all-dev
